3d677e2263f7e9b61681ec829c96d2dcd621254c
swarupa-n6 <swarupa@suntechnoservices.com>
2020-03-09 14:02:31 +0530

    This is maven

---
+++ src/main/java/com/prashanth/pashi/App.java
@@ -0,0 +1,13 @@
+package com.prashanth.pashi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
